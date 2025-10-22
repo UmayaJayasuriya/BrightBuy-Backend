c00a83ecc2b427d98fccac9036bd05cd1f270c99
Umaya Jayasuriya <jayasuriyaumaya@gmail.com>
2025-10-22 19:26:15 -0400

    updated

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd backend
-pip install -r requirements.txt
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+
+# Install Python 3
+apt-get update
+apt-get install -y python3 python3-pip
+
+# Install dependencies
+pip3 install -r backend/requirements.txt
+
+# Start backend
+uvicorn backend.app.main:app --host 0.0.0.0 --port $PORT
